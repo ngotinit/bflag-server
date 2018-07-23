15935f47cb5ad28bf8b7a3babf9061713bf4f540
Binh Nguyen Thai <nguyenthaibinh22228@gmail.com>
2018-07-23 15:27:29 +0700

    add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,29 @@ Room.create!(name: 'General')
 Room.create!(name: 'Hobby')
 Room.create!(name: 'Education')
 Room.create!(name: 'Entertainment')
+
+User.create!(
+  first_name: 'Quan',
+  last_name: 'Nguyen',
+  username: 'minhquan',
+  email: 'quan@example.com',
+  password: 'vinova123'
+)
+
+User.create!(
+  first_name: 'Thien',
+  last_name: 'Le',
+  username: 'lethien',
+  email: 'thien@example.com',
+  password: 'vinova123'
+)
+
+User.create!(
+  first_name: 'Quynh',
+  last_name: 'Do',
+  username: 'doquynh',
+  email: 'quynh@example.com',
+  password: 'vinova123'
+)
+
+
