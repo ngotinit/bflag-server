2b67bf1d00f85d3ccdc1947917b36d577a3957cc
Binh Nguyen Thai <nguyenthaibinh22228@gmail.com>
2018-07-24 11:49:18 +0700

    remove redundant lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-   paginates_per 10
   devise :database_authenticatable, :registerable,
          :recoverable, :trackable, :validatable
 
