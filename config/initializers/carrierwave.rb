529f386386c1501e78bd583c46960fc8a28e7649
Binh Nguyen Thai <nguyenthaibinh22228@gmail.com>
2018-07-25 16:12:57 +0700

    fix error of fog and carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,11 @@
 CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider:                         'Google',
+    google_storage_access_key_id:     ENV['google_storage_access_key_id'],
+    google_storage_secret_access_key: ENV['google_storage_secret_access_key']
+  }
+  config.fog_directory = ENV['BUCKET_NAME']
+
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
   end
@@ -6,11 +13,4 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage = :fog
   end
-
-  config.fog_credentials = {
-    provider:                         'Google',
-    google_storage_access_key_id:     ENV['google_storage_access_key_id'],
-    google_storage_secret_access_key: ENV['google_storage_secret_access_key']
-  }
-  config.fog_directory = ENV['BUCKET_NAME']
 end
