3d2ebaa6dae4fa1c3f27750e07f5e3be05cf10cd
Binh Nguyen Thai <nguyenthaibinh22228@gmail.com>
2018-07-30 09:02:47 +0700

    change to localtime in message entity

--- app/api/api/v1/entities/message_entity.rb
+++ app/api/api/v1/entities/message_entity.rb
@@ -3,7 +3,7 @@ module API
     module Entities
       class MessageEntity < Grape::Entity
         format_with(:datetime) do |date|
-          date.strftime('%Y-%m-%d %H:%M:%S')
+          date.localtime.strftime('%Y-%m-%d %H:%M:%S')
         end
 
         with_options(format_with: :datetime) do
